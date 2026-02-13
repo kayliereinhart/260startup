e0c278e16034b6e580ed2cadf1a8f809411b51ed
Kaylie <kayliereinhart@gmail.com>
2026-02-12 20:08:08 -0700

    Added header and footer to app

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,42 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+    return (
+        <div className="body">
+        <header class="container-fluid">
+            <nav class="navbar fixed-top">
+                <div class="navbar-brand">
+                    <td><img src="/bookstack.jpg" width="70" height="70" /></td>
+                    <td><h1>My Bookshelf</h1></td>
+                </div>
+                <menu class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.html">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="shelf.html">Bookshelf</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="addBook.html">Add Book</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="viewBook.html">View Book</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="notifications.html">Notifications</a>
+                    </li>
+                </menu>
+            </nav>
+        </header>
+
+        <main>App components go here</main>
+
+        <footer>
+            <div class="container-fluid">
+                <span class="text-reset">Kaylie Reinhart</span>
+                <a class="text-reset" href="https://github.com/kayliereinhart/260startup.git">GitHub</a>
+            </div>
+        </footer>
+    </div>
+    );
 }
