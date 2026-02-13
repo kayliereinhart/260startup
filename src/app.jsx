f4d89b3845f3f0b0f88d5def63e322c59b72ebbc
Kaylie <kayliereinhart@gmail.com>
2026-02-12 20:24:46 -0700

    Applications supports navigating to different components

--- src/app.jsx
+++ src/app.jsx
@@ -1,44 +1,63 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Shelf } from './shelf/shelf';
+import { AddBook } from './addBook/addBook';
+import { ViewBook } from './viewBook/viewBook';
+import { Notifications } from './notifications/notifications';
 
 export default function App() {
     return (
+        <BrowserRouter>
         <div className="body">
-        <header class="container-fluid">
-            <nav class="navbar fixed-top">
-                <div class="navbar-brand">
+        <header className="container-fluid">
+            <nav className="navbar fixed-top">
+                <div className="navbar-brand">
                     <td><img src="/bookstack.jpg" width="70" height="70" /></td>
                     <td><h1>My Bookshelf</h1></td>
                 </div>
-                <menu class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="index.html">Login</a>
+                <menu className="navbar-nav">
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="">Login</NavLink>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="shelf.html">Bookshelf</a>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="shelf">Bookshelf</NavLink>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="addBook.html">Add Book</a>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="addBook">Add Book</NavLink>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="viewBook.html">View Book</a>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="viewBook">View Book</NavLink>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="notifications.html">Notifications</a>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="notifications">Notifications</NavLink>
                     </li>
                 </menu>
             </nav>
         </header>
 
-        <main>App components go here</main>
+        <Routes>
+            <Route path='/' element={<Login />} exact />
+            <Route path='/shelf' element={<Shelf />} />
+            <Route path='/addBook' element={<AddBook />} />
+            <Route path='/viewBook' element={<ViewBook />} />
+            <Route path='/notifications' element={<Notifications />} />
+            <Route path='*' element={<NotFound />} />
+        </Routes>
 
         <footer>
-            <div class="container-fluid">
-                <span class="text-reset">Kaylie Reinhart</span>
-                <a class="text-reset" href="https://github.com/kayliereinhart/260startup.git">GitHub</a>
+            <div className="container-fluid">
+                <span className="text-reset">Kaylie Reinhart</span>
+                <a className="text-reset" href="https://github.com/kayliereinhart/260startup.git">GitHub</a>
             </div>
         </footer>
     </div>
+    </BrowserRouter>
     );
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
